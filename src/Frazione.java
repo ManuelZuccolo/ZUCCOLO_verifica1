511534f431f4ca220363ac86dbc5db03a43efa94
ManuelZuccolo <manuelzuccolo2007@gmail.com>
2025-10-15 08:24:04 +0200

    Implementazione della gestione dell'eccezione

--- src/Frazione.java
+++ src/Frazione.java
@@ -1,5 +1,24 @@
 public class Frazione implements Operando
 {
+    //Attributi
+    private int numeratore;
+    private int denominatore;
+
+    //Costruttori
+    //Maidenless
+    public Frazione() {}
+
+    //Questo invece li ha
+    public Frazione(int num, int den) throws DenominatoreNulloException
+    {
+        if(den == 0)
+        {
+            throw new DenominatoreNulloException("Non si può dividere per 0, tornatene alle elementari!");
+        }
+        numeratore = num;
+        denominatore = den;
+
+    }
 
     @Override
     public void stampa()
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
+        /*Step by step
+        * Chiedi frazione in input
+        * Fa controllo validità con l'eccezione (usa try catch)
+        * Usa metodi di stampa e toString per mostrare il valore (tramite calcola)
+        * Aspettare corerzione del prof e sperare di essere andato bene*/
+
+        Scanner sc = new Scanner(System.in);
+        int num, den;
+
+        System.out.println("Inserrie valore del numeratore: ");
+        num = sc.nextInt();
+        System.out.println("Inserrie valore del denominatore: ");
+        den = sc.nextInt();
+
+        try
+        {
+            Frazione fr =  new Frazione(num, den);
+
+        }catch(DenominatoreNulloException e)
+        {
+            System.out.println("Errore" + e.getMessage());
+        }
+
+
+
+
 
     }
 }
